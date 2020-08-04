9a9c51e9b0899d07b7b76b893031beb0c206040f
Senior S <maximatador23@gmail.com>
2020-08-03 22:14:51 -0300

    1.0.0

---
+++ Events.cs
@@ -0,0 +1,81 @@
+﻿using System.Threading.Tasks;
+using OpenMod.API.Eventing;
+using OpenMod.Core.Users.Events;
+using OpenMod.Core.Eventing;
+using OpenMod.API.Prioritization;
+using OpenMod.Unturned.Users;
+using OpenMod.API.Permissions;
+using Microsoft.Extensions.Configuration;
+using SDG.Unturned;
+using Serilog.Core;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace JobsOnlineUI
+{
+    public static class Jobs
+    {
+        public static int Civil;
+        public static int Medic;
+        public static int Police;
+    }
+
+    public class PlayerJoined: IEventListener<UserConnectedEvent>
+    {
+        private readonly IPermissionChecker ro_PermissionChecker;
+        private readonly IConfiguration ro_Configuration;
+
+        public PlayerJoined(IPermissionChecker permissionChecker, IConfiguration configuration)
+        {
+            ro_PermissionChecker = permissionChecker;
+            ro_Configuration = configuration;
+        }
+
+        [EventListener(Priority = Priority.Lowest)]
+        public async Task HandleEventAsync(object sender, UserConnectedEvent @event)
+        {
+            UnturnedUser user = (UnturnedUser)@event.User;
+            Jobs.Civil++;
+            PermissionGrantResult medic = await ro_PermissionChecker.CheckPermissionAsync(user, ro_Configuration.GetSection("plugin_configuration:medic_permission").Get<string>());
+            PermissionGrantResult police = await ro_PermissionChecker.CheckPermissionAsync(user, ro_Configuration.GetSection("plugin_configuration:police_permission").Get<string>());
+
+            if (medic == PermissionGrantResult.Grant && !user.Player.channel.owner.isAdmin)
+            {
+                Jobs.Medic++;
+            }
+            if (police == PermissionGrantResult.Grant && !user.Player.channel.owner.isAdmin)
+            {
+                Jobs.Police++;
+            }
+        }
+
+        public class PlayerQuit : IEventListener<UserDisconnectedEvent>
+        {
+            private readonly IPermissionChecker ro_PermissionChecker;
+            private readonly IConfiguration ro_Configuration;
+
+            public PlayerQuit(IPermissionChecker permissionChecker, IConfiguration configuration)
+            {
+                ro_PermissionChecker = permissionChecker;
+                ro_Configuration = configuration;
+            }
+
+            public async Task HandleEventAsync(object sender, UserDisconnectedEvent @event)
+            {
+                UnturnedUser user = (UnturnedUser)@event.User;
+                Jobs.Civil--;
+                var medic = await ro_PermissionChecker.CheckPermissionAsync(user, ro_Configuration.GetSection("plugin_configuration:medic_permission").Get<string>());
+                var police = await ro_PermissionChecker.CheckPermissionAsync(user, ro_Configuration.GetSection("plugin_configuration:police_permission").Get<string>());
+
+                if (medic == PermissionGrantResult.Grant && !user.Player.channel.owner.isAdmin && Jobs.Medic > 0)
+                {
+                    Jobs.Medic--;
+                }
+                if (police == PermissionGrantResult.Grant && !user.Player.channel.owner.isAdmin && Jobs.Police > 0)
+                {
+                    Jobs.Police--;
+                }
+            }
+        }
+    }
+}
---
+++ JobsOnlineUI.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Cysharp.Threading.Tasks;
+using OpenMod.Unturned.Plugins;
+using OpenMod.API.Plugins;
+using SDG.Unturned;
+using OpenMod.API.Permissions;
+using System.Drawing;
+
+[assembly: PluginMetadata("SS.JobsOnlineUI", DisplayName = "JobsOnlineUI")]
+namespace JobsOnlineUI
+{
+    public class JobsOnlineUI : OpenModUnturnedPlugin
+    {
+        private readonly ILogger<JobsOnlineUI> ro_Logger;
+        private readonly IConfiguration ro_configuration;
+        private readonly IPermissionRegistry ro_permissionRegistry;
+
+        public JobsOnlineUI(
+            ILogger<JobsOnlineUI> logger,
+            IConfiguration configuration,
+            IPermissionRegistry permissionRegistry,
+            IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            ro_Logger = logger;
+            ro_configuration = configuration;
+            ro_permissionRegistry = permissionRegistry;
+        }
+
+        protected override async UniTask OnLoadAsync()
+        {
+			await UniTask.SwitchToMainThread();
+            PlayerInput.onPluginKeyTick += OnKeyPressed;
+            ro_permissionRegistry.RegisterPermission(this, ro_configuration.GetSection("plugin_configuration:medic_permission").Get<string>());
+            ro_permissionRegistry.RegisterPermission(this, ro_configuration.GetSection("plugin_configuration:police_permission").Get<string>());
+            ro_Logger.LogInformation("Plugin loaded correctly!");
+            ro_Logger.LogInformation("If you have any error you can contact the owner in discord: Senior S#9583");
+        }
+
+        private void OnKeyPressed(Player player, uint simulation, byte key, bool state)
+        {
+            if (key == 4 && state)
+            {
+                EffectManager.sendUIEffect(28000, 280, player.channel.owner.playerID.steamID, true, Jobs.Civil + "", Jobs.Police + "", Jobs.Medic + "");
+            }
+            else if (key == 4 && !state)
+            {
+                EffectManager.askEffectClearByID(28000, player.channel.owner.playerID.steamID);
+            }
+        }
+
+        protected override async UniTask OnUnloadAsync()
+        {
+            await UniTask.SwitchToMainThread();
+            PlayerInput.onPluginKeyTick -= OnKeyPressed;
+            ro_Logger.LogInformation("Plugin unloaded correctly!");
+        }
+    }
+}
